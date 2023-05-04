c14592135bed4291c46a1329fe2d56f1dd47a0b4
nevmerzhitskaya <lamara@tut.by>
2023-05-04 20:04:11 +0200

    feat: make 03-files-in-folder task. Read info about files in the secret-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fsPromises = require('node:fs/promises');
+const { stat } = require('fs');
+
+const path = require('path');
+const filePath = path.join('03-files-in-folder/', 'secret-folder');
+
+const showFileList = async () => {
+  try {
+    const fileList = await fsPromises.readdir(filePath, { withFileTypes: true });
+
+    for (const file of fileList) {
+      if (!file.isDirectory()) {
+        stat(path.join('03-files-in-folder/', 'secret-folder', file.name), (err, stats) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(`${path.parse(file.name).name} - ${path.extname(file.name).substring(1)} - ${stats.size} bytes`);
+          }
+        });
+      }
+    }
+  } catch (error) {
+    console.error(error.name + ':', 'Operation failed');
+  }
+}
+
+showFileList();
